eaccf9807d3ec59f5c9f9a7a0e3d4a3a236b18e7
juuliankim <71047720+juuliankim@users.noreply.github.com>
2021-08-09 19:17:49 -0300

    Add files via upload

---
+++ server.js
@@ -0,0 +1,78 @@
+const express = require('express')
+const app = express()
+const http = require('http')
+const server = http.Server(app)
+const io = require('socket.io')(server)
+const normalize = require('normalizr').normalize
+const schema = require('normalizr').schema
+const productos = require('./api/productos')
+
+require('./database/connection')
+
+app.use(express.json())
+app.use(express.urlencoded({ extended: true }))
+
+app.use(express.static('public'))
+
+app.use((err, req, res, next) =>{
+    console.error(err.message)
+    return res.status(500).send('Algo se rompió!!')
+})
+
+const productosRouter = require('./routes/productosRouter')
+app.use('/api', productosRouter)
+const mensajesRouter = require('./routes/mensajesRouter')
+app.use('/api', mensajesRouter)
+
+io.on('connection', async socket => {
+    console.log('Usuario conectado')
+
+    socket.on('nuevo-producto', nuevoProducto => {
+        console.log(nuevoProducto)
+        productos.guardar(nuevoProducto)
+    })
+    socket.emit('guardar-productos', () => {
+        socket.on('notificacion', data => {
+            console.log(data)
+        })
+    }) 
+    //socket.emit('actualizar-tabla', await productos.listar())
+
+    socket.on("new-message", async function (data) {
+
+        await Mensajes.guardar(data)
+
+        let mensajesDB = await Mensajes.getAll()     
+
+        const autorSchema = new schema.Entity('autor', {}, { idAttribute: 'nombre' });
+
+        const mensajeSchema = new schema.Entity('texto', {
+            autor: autorSchema
+        }, { idAttribute: '_id' })
+
+        const mensajesSchema = new schema.Entity('mensajes', {
+            msjs: [mensajeSchema]
+        }, {idAttribute: 'id'})
+
+        const mensajesNormalizados = normalize(mensajesDB, mensajesSchema)
+                
+        messages.push(mensajesDB);
+
+        console.log(mensajesDB)
+
+        console.log(mensajesNormalizados)
+            
+        io.sockets.emit("messages", mensajesNormalizados)
+    })
+
+})
+
+const PORT = 8080
+
+const svr = server.listen(PORT, () => {
+    console.log(`servidor escuchando en http://localhost:${PORT}`)
+})
+
+server.on('error', error => {
+    console.log('error en el servidor:', error)
+})
